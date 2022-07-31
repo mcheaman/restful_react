7f16be1d57c0ae9b82f0f19ef9229380a6349516
Mason <admin@MacPro.local>
2022-07-31 09:10:54 -0700

    api requests with axios started

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,31 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles.css';
 import Person from './Person';
-const Results = () => {
-    const getAll = (): Person[]  => {
+import axios from 'axios';
+// const axios = require('axios').default;
+
+const Results: React.FC = () => {
+    const [people, setPeople] = useState<Person[]>([])
+    const getAll = () => {
         //make api call
         //sample objects
-        const samples = [
-            {
-                firstName: 'Mason',
-                lastName: 'Heaman',
-                occupation: 'SWE',
-                age: '21'
-            },
-            {
-                firstName: 'Nicole',
-                lastName: 'Lynch',
-                occupation: 'Nail Tech',
-                age: '21'
-            }
-        ];
-        return samples;
+        // const samples: Person[] = [
+        //     {
+        //         firstName: 'Mason',
+        //         lastName: 'Heaman',
+        //         occupation: 'SWE',
+        //         age: '21'
+        //     },
+        //     {
+        //         firstName: 'Nicole',
+        //         lastName: 'Lynch',
+        //         occupation: 'Nail Tech',
+        //         age: '21'
+        //     }
+        // ];
+        axios.get('localhost:1337/api/people')
+            .then(response => {
+                alert(response)
+            })
+        // setPeople(samples)
+        return;
     }
   return (
     <div>
         <h2 className='result_header'>Results</h2>
+        <button onClick={() => getAll()}>Refresh</button>
         <ul className='result_container'>
-            {getAll().map(person => {
+            {people.map(person => {
                 return(
                     <li className='result'>{`${person.firstName} ${person.lastName}, ${person.age}, ${person.occupation}`}</li>
             )})}
